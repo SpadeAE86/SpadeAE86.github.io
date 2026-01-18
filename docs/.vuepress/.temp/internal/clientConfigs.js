e27cb38f9bc937e703b6c3060b4e4541fa4bd96e
NuochengLin <nuochel1@uci.edu>
2026-01-18 22:47:11 +0800

    start

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,27 @@
+import * as clientConfig0 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-hea_b85d27ef61a528a9a186d0b89e674a10/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-to_c80d14228663cdb390f3d07435404748/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@_4bfe4cf1b5f9c1e7dc62cb2214ed160d/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-h_4ed5ce611239a9d77eada415c55627c1/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from 'C:/Blog/vuepress-starter/docs/.vuepress/.temp/git/config.js'
+import * as clientConfig5 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoo_d64d221d0e680a7634c39b241cfed25f/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig6 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@_e0e7b2555d57a2dd7ce4f9de0e75e1a3/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig7 from 'C:/Blog/vuepress-starter/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig8 from 'C:/Blog/vuepress-starter/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig9 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data_d6cb6973a6de6a1b8711fade0198965b/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig10 from 'C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0_cac4d63d06e4bab561ca604032eb58f4/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig11 from 'C:/Blog/vuepress-starter/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+  clientConfig11,
+].map((m) => m.default).filter(Boolean)
---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,13 @@
+import "C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helpe_14afbcaa3a808c07d0df4c82c0b22735/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-prismjs@2._7a6abc51770d74e4ee1cb346ea70c645/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helpe_14afbcaa3a808c07d0df4c82c0b22735/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helpe_14afbcaa3a808c07d0df4c82c0b22735/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helpe_14afbcaa3a808c07d0df4c82c0b22735/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helpe_14afbcaa3a808c07d0df4c82c0b22735/node_modules/@vuepress/highlighter-helper/lib/client/index.js"
+import "C:/Blog/vuepress-starter/node_modules/.pnpm/@vuepress+highlighter-helpe_14afbcaa3a808c07d0df4c82c0b22735/node_modules/@vuepress/highlighter-helper/lib/client/styles/code-block-title.css"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
